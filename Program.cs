602ae8f775ff507ebb0031ecb28962bed9678387
Paulo Paes <paulo.c.s.paes@hotmail.com>
2025-05-22 15:40:08 -0300

    Ajuste para funcionar no windows 11 com invariant culture no Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var culture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentCulture = culture;
 CultureInfo.DefaultThreadCurrentUICulture = culture;
-culture.NumberFormat.NumberDecimalSeparator = ".";
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<SharedDataService>();
